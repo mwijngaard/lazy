d53e2f2958c4e08837b6ecdd11c25379d684288c
Merijn Wijngaard <mwijngaard@gmail.com>
2015-10-23 10:10:32 +0200

    Added sample test case for range

---
+++ test/Lazy/RangeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace mwijngaard\Lazy;
+
+use Eris\TestTrait;
+use Eris\Generator;
+
+class RangeTest extends \PHPUnit_Framework_TestCase {
+	use TestTrait;
+
+	public function testRangeIsEqualToNative() {
+		$this->forAll(Generator\int(), Generator\int())->__invoke(function ($start, $end) {
+			if ($end < $start) {
+				list($end, $start) = array($start, $end);
+			}
+			$this->assertEquals(range($start, $end), lazy_range($start, $end)->resolve());
+		});
+	}
+}
