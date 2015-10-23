7c44be9be05a945495f3327b103403b3450f1065
Merijn Wijngaard <mwijngaard@gmail.com>
2015-10-23 14:02:51 +0200

    added new functions to autoload file

--- src/autoload.php
+++ src/autoload.php
@@ -1,16 +1,25 @@
 <?php
 
 $files = [
+	'Lazy/LazyAnd.php',
 	'Lazy/LazyConcat.php',
 	'Lazy/LazyCount.php',
 	'Lazy/LazyCycle.php',
+	'Lazy/LazyEmpty.php',
+	'Lazy/LazyException.php',
 	'Lazy/LazyFilter.php',
+	'Lazy/LazyIf.php',
 	'Lazy/LazyIterate.php',
 	'Lazy/LazyMap.php',
+	'Lazy/LazyMax.php',
+	'Lazy/LazyMin.php',
+	'Lazy/LazyOr.php',
+	'Lazy/LazyProduct.php',
 	'Lazy/LazyRange.php',
 	'Lazy/LazyReduce.php',
 	'Lazy/LazyRepeat.php',
-	'Lazy/LazyReplicate.php'
+	'Lazy/LazyReplicate.php',
+	'Lazy/LazySum.php',
 ];
 
 foreach ($files as $file) {
